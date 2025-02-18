82c7c6ef48e75c72c18c5e796865134d642d9fbd
Daniel Tull <dt@danieltull.co.uk>
2025-02-18 10:28:29 +0000

    Initial Commit

---
+++ Scroll Updates Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scroll Updates Test
+//
+//  Created by Daniel Tull on 18/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scroll Updates Test/Scroll_Updates_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_Updates_TestApp.swift
+//  Scroll Updates Test
+//
+//  Created by Daniel Tull on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_Updates_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
